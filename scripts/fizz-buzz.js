ca602dd4a6685cc500d2b0f47b892020fcc7c6a3
michodgs25 <michodgs@gmail.com>
2021-03-06 11:16:57 +0000

    Apply continue& modulo statement and operand to fizz test

--- scripts/fizz-buzz.js
+++ scripts/fizz-buzz.js
@@ -1,3 +1,10 @@
 for (var i = 0; i < 100; i++) {
+    if (i % 3 === 0) {
+        console.log('Fizz')
+        continue
+    }
+    if (i % 5 === 0) {
+        console.log('Buzz')
+    }
     console.log(i)
 }
