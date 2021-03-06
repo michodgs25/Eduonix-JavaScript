245f6c37b0180711f943d7034c58cb23c1551066
michodgs25 <michodgs@gmail.com>
2021-03-06 12:16:03 +0000

    Create fizz buzz test as a global variable

--- scripts/fizz-buzz.js
+++ scripts/fizz-buzz.js
@@ -1,10 +1,20 @@
+// Fizz Buzz loop, fizz, buzz and fizzbuzz replace seleced integers
+
 for (var i = 0; i < 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz')
+        continue
+    }
     if (i % 3 === 0) {
         console.log('Fizz')
         continue
     }
     if (i % 5 === 0) {
         console.log('Buzz')
+        continue
     }
     console.log(i)
 }
+
+// shortest fizz-buzz, defined as a global variable
+for(i=0;i++<100;)console.log(((i%3?'':'Fizz')+(i%5?'':'Buzz'))|| i)
