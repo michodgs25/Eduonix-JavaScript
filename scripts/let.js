08f54f7ffab29fd7e1311221b6d0db67574be6ac
michodgs25 <michodgs@gmail.com>
2021-03-20 13:13:00 +0000

    Create const and let features

---
+++ scripts/let.js
@@ -0,0 +1,12 @@
+var people = "Michael";
+
+function sayMyName() {
+    console.log(people);
+}
+
+function myNameIs() {
+    var name = "Greg";
+    console.log(people)
+}
+sayMyName();
+console.log(people);
